eab6a36d77770aeebb0556b6d4b096c76c09ce50
Tim La <tlamarketing111@gmail.com>
2022-03-28 19:34:31 -0400

    able to fetch data of one specific crypto currency

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -5,12 +5,18 @@ import millify from 'millify';
 import { Col, Row, Typography, Select } from 'antd';
 import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
 
+import { useGetCryptoDetailsQuery } from '../services/cryptoAPI';
+
 const { Title, Text } = Typography;
 const { Option } = Select;
 
 const CryptoDetails = () => {
     const { coinId } = useParams();
     const [timePeriod, setTimePeriod] = useState("7d");
+    const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
+
+    console.log(data);
+
     return (
         <div>
             CryptoDetails {coinId}
