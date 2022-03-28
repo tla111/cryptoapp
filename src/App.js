ec08053298e5b803aa4ddb0fcad90f181c2755b7
Tim La <tlamarketing111@gmail.com>
2022-03-28 10:39:12 -0400

    added routes & import components in app component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 import './App.css';
 
-import { Navbar } from './components';
+import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components';
 
 const App = () => {
     return (
@@ -12,7 +12,27 @@ const App = () => {
                 <Navbar />
             </div>
             <div className="main">
-
+                <Layout>
+                    <div className="routes">
+                        <Routes>
+                            <Route exact path="/">
+                                <Homepage />
+                            </Route>
+                            <Route exact path="/exchanges">
+                                <Exchanges />
+                            </Route>
+                            <Route exact path="/cryptocurrencies">
+                                <Cryptocurrencies />
+                            </Route>
+                            <Route exact path="/crypto/:coinId">
+                                <CryptoDetails />
+                            </Route>
+                            <Route exact path="/news">
+                                <News />
+                            </Route>
+                        </Routes>
+                    </div>
+                </Layout>
             </div>
             <div className="footer">
 
