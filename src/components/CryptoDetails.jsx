fc0ec3ee1b60451903eb1c96cd13b5e5a9e29226
Tim La <tlamarketing111@gmail.com>
2022-03-28 19:47:42 -0400

    added stats title

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -52,7 +52,24 @@ const CryptoDetails = () => {
                 {time.map((date) => <Option key={date}>{date}</Option>)}
             </Select>
             {/* line chart */}
-
+            <Col className="stats-container">
+                <Col className="coin-value-statistics">
+                    <Col className="coin-value-statistics-heading">
+                        <Title level={3} className="coin-details-heading">
+                            {cryptoDetails.name} Value Statistics
+                        </Title>
+                        <p>An overview showing the statistics of {cryptoDetails.name}, such as the base and quote currency, the rank, and trading volume.</p>
+                    </Col>
+                    {stats.map(({ icon, title, value }) => (
+                        <Col className="coin-stats">
+                            <Col className="coin-stats-name">
+                                <Text>{icon}</Text>
+                                <Text>{title}</Text>
+                            </Col>
+                        </Col>
+                    ))}
+                </Col>
+            </Col>
         </Col>
     )
 }
