feefbe53f5be0331688ec1190d319ac96a17543d
Tim La <tlamarketing111@gmail.com>
2022-03-28 10:31:35 -0400

    added router in index file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Button, Menu, Typography, Avatar } from 'antd';
-import { Link, Navigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 
 import icon from '../images/cryptocurrency.png';
