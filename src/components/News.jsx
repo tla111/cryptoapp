20389f45043ebda6bca55d4b00d3ddcf7915b067
Tim La <tlamarketing111@gmail.com>
2022-03-28 12:37:35 -0400

    insert image and description

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -7,6 +7,7 @@ import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 const { Text, Title } = Typography;
 const { Option } = Select;
 
+const demoImage = "https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News"
 
 const News = ({ simplified }) => {
     const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: "Cryptocurrency", count: simplified ? 6 : 12 })
@@ -21,7 +22,13 @@ const News = ({ simplified }) => {
                         <a href={news.url} target="_blank" rel="noreferrer">
                             <div className="news-image-container">
                                 <Title className="news-title" level={4}>{news.name}</Title>
+                                <img src={news?.image?.thumbnail?.contentUrl || demoImage} alt="news" />
                             </div>
+                            <p>
+                                {news.description > 100 ? `${news.description.substring(0, 100)}...`
+                                    : news.description
+                                }
+                            </p>
                         </a>
                     </Card>
                 </Col>
