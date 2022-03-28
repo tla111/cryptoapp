b6cbfa3eb1a3fc856c271c21c3763d6d49c85b76
Tim La <tlamarketing111@gmail.com>
2022-03-28 11:28:35 -0400

    able to retrieve data

--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -1,11 +1,11 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
     'X-RapidAPI-Key': '248d0d31e6msh3dbc20f058754e0p1d102cjsn3059651e6587'
 }
 
-const baseUrl = "https://coinranking1.p.rapidapi.com/exchanges";
+const baseUrl = "https://coinranking1.p.rapidapi.com";
 
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
 
